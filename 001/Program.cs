83f0dc5fca3a78a1f6c33010f8ad88aeddfb2050
Nataliia <nvasinaa@gmail.com>
2022-07-19 21:53:35 +0300

    задача 53

---
+++ 001/Program.cs
@@ -0,0 +1,50 @@
+﻿// задать двумерный массив и поменять первую строку и последнюю
+void Zadacha53()
+{
+    Random random = new Random ();
+    int rows = 3; 
+    int columns = 4; 
+    int [,] array = new int [rows, columns];
+     FillArray(array);
+     PrintArray(array);
+     ChangeArray(array);
+     PrintArray(array);
+
+    void FillArray (int [,] array)
+    { 
+    for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++) 
+       {
+        array[i, j] = random.Next (1, 10); 
+       }
+    }
+     }
+
+     void PrintArray (int [,] array)
+     {
+       for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++) 
+       {
+        Console.Write(array[i, j] + "\t");
+       }
+       Console.WriteLine();
+     }  
+     }
+      
+     void ChangeArray(int[,] array)
+{
+    int temp = 0;
+    for (int j=0; j < columns; j++)
+        {
+            temp = array[0, j];
+             array[0, j] = array[rows - 1, j];
+             array[rows - 1, j] = temp;
+        }
+        Console.WriteLine();
+
+}
+}
+
+Zadacha53();
