3ec7da6994ad0d97ff75c9ab41a000e6e633f5e0
Nataliia <nvasinaa@gmail.com>
2022-07-19 21:55:06 +0300

    задача 57

---
+++ 003/Program.cs
@@ -0,0 +1,51 @@
+﻿// составить частотный словарь 
+
+void Zadacha57()
+{
+    Random random = new Random ();
+    int rows = 3; 
+    int columns = 3; 
+    int [,] array = new int [rows, columns];
+    int [] d = new int [10];
+     FillArray(array);
+     PrintArray(array);
+
+    for (int i=0; i < rows; i++)
+    {
+      for (int j=0; j < columns; j++)
+        {
+            d[array[i, j]]++;     
+        }  
+    }
+    for (int i=0; i < d.Length; i++)
+    {
+        Console.WriteLine($"эллемент {i} встречается {d [i]} раз");
+    }
+
+    void FillArray (int [,] array)
+    { 
+    for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++) 
+       {
+        array[i, j] = random.Next (0, 10); 
+       }
+    }
+     }
+
+     void PrintArray (int [,] array)
+     {
+       for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++) 
+       {
+        Console.Write(array[i, j] + "\t");
+       }
+       Console.WriteLine();
+     }  
+     }
+      
+     
+}
+
+Zadacha57();
